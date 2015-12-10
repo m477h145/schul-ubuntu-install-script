87bc0c07a2d51993948b473e97bc33c8c88b96dd
Matthias <matthias0608@live.de>
2015-12-11 00:23:26 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-add-repository -y ppa:libreoffice/ppa
+sudo apt-add-repository -y ppa:webupd8team/y-ppa-manager
+sudo apt-add-repository -y ppa:numix/ppa
+wget -O - http://www.geogebra.net/linux/office@geogebra.org.gpg.key| apt-key add - 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y vlc libreoffice y-ppa-manager geogebra44 scratch numix-icon-theme-circle firefox gimp audacity freemind inkscape librecad avidemux italc-master lame winbind libpam-winbind libpam-mount libnss-winbind samba-common-bin network-manager isc-dhcp-client cifs-utils smbclient cups
