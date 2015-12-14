3869e95ca6a7c31f8e7d4ed644a6ddb7ddac7568
Matthias <matthias0608@live.de>
2015-12-14 17:32:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ wget -O - http://www.geogebra.net/linux/office@geogebra.org.gpg.key| apt-key add
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y vlc libreoffice y-ppa-manager geogebra44 scratch numix-icon-theme-circle firefox gimp audacity freemind inkscape librecad avidemux italc-master lame winbind libpam-winbind libpam-mount libnss-winbind samba-common-bin network-manager isc-dhcp-client cifs-utils smbclient cups curl
-curl https://update.iserv.eu/debian/pool/main/i/iserv-client/iserv-client_15.10.20-1_all.deb
-dpkg -i iserv-client_15.10.20-1_all.deb
+wget https://update.iserv.eu/debian/pool/main/i/iserv-client/iserv-client_15.10.20-1_all.deb
+sudo dpkg -i iserv-client_15.10.20-1_all.deb
 cd /etc/
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/rc.local
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/nsswitch.conf
