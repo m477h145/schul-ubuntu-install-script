ec483e8e6d23b7ec9da3c47c686cbe7d223a887e
Matthias <matthias0608@live.de>
2015-12-14 18:12:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ sudo apt-add-repository -y ppa:numix/ppa
 wget -O - http://www.geogebra.net/linux/office@geogebra.org.gpg.key| apt-key add - 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y vlc libreoffice y-ppa-manager geogebra44 scratch numix-icon-theme-circle firefox gimp audacity freemind inkscape librecad avidemux italc-master lame winbind libpam-winbind libpam-mount libnss-winbind samba-common-bin network-manager isc-dhcp-client cifs-utils smbclient cups curl
-wget https://update.iserv.eu/debian/pool/main/i/iserv-client/iserv-client_15.10.20-1_all.deb
-sudo dpkg -i iserv-client_15.10.20-1_all.deb
+sudo apt-get install -y vlc libreoffice y-ppa-manager geogebra5 scratch numix-icon-theme-circle firefox gimp audacity freemind inkscape librecad avidemux italc-master lame winbind libpam-winbind libpam-mount libnss-winbind samba-common-bin network-manager isc-dhcp-client cifs-utils smbclient cups curl gedit
+#curl -L -O https://update.iserv.eu/debian/pool/main/i/iserv-client/iserv-client_15.10.20-1_all.deb
+#sudo dpkg -i iserv-client_15.10.20-1_all.deb
 cd /etc/
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/rc.local
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/nsswitch.conf
@@ -18,3 +18,6 @@ cd /etc/security/
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/pam_mount.conf.xml
 cd /etc/samba/
 curl -L -O https://github.com/m477h145/schul-ubuntu-install-script/raw/master/smb.conf
+echo "Auto-download broken"
+echo "Please download Iserv-Client manually https://update.iserv.eu/debian/pool/main/i/iserv-client/"
+echo "Then run it by: sudo dpkg -i [Filename].deb"
